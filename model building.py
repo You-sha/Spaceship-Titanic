79bc9dc7f47b4f16d47146cd3cf74aeeb6e19ed8
Yousha <123200960+You-sha@users.noreply.github.com>
2023-02-15 18:31:34 +0530

    More models and tuning

--- model building.py
+++ model building.py
@@ -19,7 +19,7 @@ df_train.dtypes
 
 features = ['HomePlanet', 'CryoSleep', 'Destination', 'Age', 'VIP',
             'RoomService', 'FoodCourt', 'ShoppingMall', 'Spa', 'VRDeck', 
-            'Grouped', 'Deck', 'Side', 'Has_expenses', 'Is_Embryo']
+            'Grouped', 'Deck', 'Has_expenses', 'Side', 'Is_Embryo']
 
 X = pd.get_dummies(df_train[features])
 y = df_train['Transported']
@@ -30,7 +30,12 @@ model = LogisticRegression(max_iter=10000)
 model.fit(X_train,y_train)
 model.score(X_test,y_test)  # 80%
 
+model2 = LogisticRegression(max_iter=10000)
+model2.fit(X,y)
+model2.score(X,y)
+
 y_pred_log = model.predict(pd.get_dummies(df_test[features]))
+y_pred_log2 = model2.predict(pd.get_dummies(df_test[features]))
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
@@ -41,8 +46,6 @@ knn = KNeighborsClassifier()
 param_grid = {'n_neighbors':np.arange(2,15)}
 knn_gscv = GridSearchCV(knn, param_grid, cv=5)
 knn_gscv.fit(X,y)
-
-
 knn2 = KNeighborsClassifier(n_neighbors=knn_gscv.best_params_['n_neighbors'])
 knn2.fit(X,y)
 knn2.score(X,y) # 81.4%
@@ -68,6 +71,10 @@ y_pred2 = model_out.predict(df_test)
 Logist_out = pd.DataFrame({'PassengerId':df_test.PassengerId, 'Transported': y_pred_log})
 Logist_out.to_csv('logist_pred.csv',index=False)
 
+Logist_out2 = pd.DataFrame({'PassengerId':df_test.PassengerId, 'Transported': y_pred_log2})
+Logist_out2.to_csv('logist_pred2.csv',index=False)
+
+
 knn_out = pd.DataFrame({'PassengerId':df_test.PassengerId, 'Transported': y_pred_knn})
 knn_out.to_csv('knn_pred.csv',index=False)
 
@@ -75,7 +82,50 @@ rf_out = pd.DataFrame({'PassengerId':df_test.PassengerId, 'Transported': y_pred_
 rf_out.to_csv('rf_pred.csv',index=False)
 
 
+from sklearn.ensemble import GradientBoostingClassifier
+gbr = GradientBoostingClassifier(random_state = 1)
+  
+# Fit to training set
+gbr.fit(X, y)
+gbr.score(X,y)
+pred_y_gbr = gbr.predict(pd.get_dummies((df_test[features])))
 
+gbr_out = pd.DataFrame({'PassengerId':df_test.PassengerId, 'Transported': pred_y_gbr})
+gbr_out.to_csv('gbr_pred.csv',index=False)
+
+from xgboost import XGBClassifier
+xgb = XGBClassifier()
+xgb.fit(X,y)
+xgb.score(X,y)
+
+y_pred_xgb = xgb.predict(pd.get_dummies((df_test[features])))
+
+xgb_out = pd.DataFrame({'PassengerId':df_test.PassengerId, 'Transported':y_pred_xgb.astype('bool')})
+xgb_out.to_csv('xgb_pred.csv',index=False)
+
+gbc = GradientBoostingClassifier()
+parameters = {
+    "n_estimators":[5,50,100],
+    "max_depth":[1,3,5],
+    "learning_rate":[0.01,0.1,1]
+}
+
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+
+cv = RandomizedSearchCV(gbc, parameters, n_iter=27, scoring='accuracy', n_jobs=-1, cv=5, random_state=1)
+cv.fit(X,y)
+cv.best_params_
+
+gbc1 = GradientBoostingClassifier(n_estimators=50,max_depth=5,learning_rate=0.1)
+
+gbc1.fit(X,y)
+gbc1.score(X,y)
+pred_y_gbr2 = gbc1.predict(pd.get_dummies((df_test[features])))
+
+gbc_out = pd.DataFrame({'PassengerId':df_test.PassengerId, 'Transported':pred_y_gbr2})
+gbc_out.to_csv('gbr_pred2.csv',index=False)
 
 
 
